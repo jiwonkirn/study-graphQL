879f95d0bf586e41f413946435a930b8ef293800
jiwonkirn <jhd1925@gmail.com>
2019-12-22 16:53:53 +0900

    Section02 Lecture 15. Operation arguments

--- grapgql-basics/src/index.js
+++ grapgql-basics/src/index.js
@@ -1,10 +1,14 @@
-import { GraphQLServer } from "graphql-yoga";
+import {
+  GraphQLServer
+} from "graphql-yoga";
 
 // String, Boolean, Int, Float, ID,
 
 // Type definition (schema)
 const typeDefs = `
   type Query {
+    greeting(name: String, position: String): String!
+    add(a: Float!, b: Float!): Float!
     me: User!
     post: Post!
   }
@@ -27,6 +31,16 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
+    greeting(parent, args, ctx, info) {
+      if (args.name && args.position) {
+        return `Hello, ${args.name}! You are my favorite ${args.position}`
+      } else {
+        return `Hello`
+      }
+    },
+    add(parent, args) {
+      return args.a + args.b
+    },
     me() {
       return {
         id: "abc123",
