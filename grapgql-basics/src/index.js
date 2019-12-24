ac4fec412b1b37bd9ff6e99e8653898bafd6dd00
jiwonkirn <jhd1925@gmail.com>
2019-12-24 19:11:39 +0900

    Section03, Lec 26-27 The Input Type

--- grapgql-basics/src/index.js
+++ grapgql-basics/src/index.js
@@ -91,9 +91,28 @@ const typeDefs = `
   }
 
   type Mutation {
-    createUser(name: String!, email: String!, age: Int): User!
-    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-    createComment(text: String!, post: ID!, author: ID!): Comment!
+    createUser(data: CreateUserInput!): User!
+    createPost(data: CreatePostInput!): Post!
+    createComment(data: CreateCommentInput!): Comment!
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  input CreatePostInput {
+    title: String!
+    body: String!
+    published: Boolean!
+    author: ID!
+  }
+
+  input CreateCommentInput {
+    text: String!
+    author: ID!
+    post: ID!
   }
 
   type User {
@@ -167,29 +186,24 @@ const resolvers = {
   },
   Mutation: {
     createUser(parent, args, ctx, info) {
-      const emailTaken = users.some(user => user.email === args.email);
+      const emailTaken = users.some(user => user.email === args.data.email);
       if (emailTaken) throw new Error("Email taken.");
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age
+        ...args.data
       };
       users.push(user);
 
       return user;
     },
     createPost(parent, args, ctx, info) {
-      const userExist = users.some(user => user.id === args.author);
+      const userExist = users.some(user => user.id === args.data.author);
       if (!userExist) throw new Error("User not found.");
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author
+        ...args.data
       };
 
       posts.push(post);
@@ -197,19 +211,17 @@ const resolvers = {
       return post;
     },
     createComment(parent, args, ctx, info) {
-      const userExist = users.some(user => user.id === args.author);
+      const userExist = users.some(user => user.id === args.data.author);
       if (!userExist) throw new Error("Unable to find user");
 
       const postConfirm = posts.some(
-        post => post.id === args.post && post.published
+        post => post.id === args.data.post && post.published
       );
       if (!postConfirm) throw new Error("Post is not exist or published");
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post
+        ...args.data
       };
 
       comments.push(comment);
